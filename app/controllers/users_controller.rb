2f634ae996a4833daf5fe1f5b3e0f8bf7cb73150
Parker Parham <parkerparham@gmail.com>
2022-12-28 17:26:45 -0500

    Adds route for user auto-login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
+    skip_before_action :authorized, only: :create
+
     def create
         user = User.create!(user_params)
         render json: user
+    end
+
+    def show
+        current_user = User.find(session[:user_id])
+        render json: current_user
     end
 
     private
