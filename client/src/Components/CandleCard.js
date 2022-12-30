4f1f9e747fc87ac13a5898f4a5fc14814737de78
Parker Parham <parkerparham@gmail.com>
2022-12-30 15:11:20 -0500

    shows users likes on client

--- client/src/Components/CandleCard.js
+++ client/src/Components/CandleCard.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
-export default function CandleCard({ candle }) {
+export default function CandleCard({ candle, favoriteCandles }) {
 
     const { name, notes, price, producer, image_url, size } = candle
     // console.log(candle.favorites.length)
+    // console.log('from card', favoriteCandles)
+    // console.log('test from card', favoriteCandles.includes({name}), name)
+    // console.log(name === favoriteCandles[0], name, favoriteCandles[0])
+
+    let liked = favoriteCandles.includes(name)
+
 
   return (
     <div className='candle-card'>
@@ -12,7 +18,7 @@ export default function CandleCard({ candle }) {
         <p><i>Notes: {notes}</i></p>
         <p className='card-info'>{price} | {size} | {producer}</p>
         <div className='card-engage'>
-          <div className='card-favorite'><i className="fa-solid fa-heart"></i></div>
+          <div className='card-favorite'>{liked ? <i className="fa-solid fa-heart"></i> :  <i className="fa-regular fa-heart"></i>}</div>
           <div className='card-review'>Review</div>
         </div>
     </div>
--- client/src/Components/CandleContainer.js
+++ client/src/Components/CandleContainer.js
@@ -3,7 +3,7 @@ import CandleCard from './CandleCard'
 
 export default function CandleContainer({candles, favoriteCandles }) {
   const displayCandleCards = candles.map((candle) => {
-    return <CandleCard key={candle.id} candle={candle}/>
+    return <CandleCard favoriteCandles={favoriteCandles} key={candle.id} candle={candle}/>
   })
 
 
